47ef2c60c0f3019b847ce72c9655da6f7bf3c6e0
auriaksLT <aurimascss@gmail.com>
2022-01-18 17:43:48 +0000

    +formValidation

--- app/Http/Controllers/API/StudentController.php
+++ app/Http/Controllers/API/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -32,38 +33,81 @@ class StudentController extends Controller
         $student->delete();
         return response()->json([
             'status' => 200,
-            'message' => 'Student Deleted Successfully',
+            'message' => 'User Deleted Successfully',
         ]);
     }
 
 
     public function store(Request $request)
     {
-        $student = new Student;
-        $student->name = $request->input('name');
-        $student->course = $request->input('course');
-        $student->email = $request->input('email');
-        $student->phone = $request->input('phone');
-        $student->save();
-
-        return response()->json([
-            'status' => 200,
-            'message' => 'Student Added Successfully!',
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required|max:191',
+            'course'=> 'required|max:191',
+            'email'=> 'required|email|max:191',
+            'phone'=> 'required|numeric|min:10|max:10',
         ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'validate_err' => $validator->messages(),
+            ]);
+        }
+        else
+        {
+            $student = new Student;
+            $student->name = $request->input('name');
+            $student->course = $request->input('course');
+            $student->email = $request->input('email');
+            $student->phone = $request->input('phone');
+            $student->save();
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'User Added Successfully!',
+            ]);
+        }
+
+        
     }
 
     public function update(Request $request, $id)
     {
-        $student = Student::find($id);
-        $student->name = $request->input('name');
-        $student->course = $request->input('course');
-        $student->email = $request->input('email');
-        $student->phone = $request->input('phone');
-        $student->update();
-
-        return response()->json([
-            'status' => 200,
-            'message' => 'Student Updated Successfully!',
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required|max:191',
+            'course'=> 'required|max:191',
+            'email'=> 'required|email|max:191',
+            'phone'=> 'required|numeric|min:10',
         ]);
+
+        $student = Student::find($id);
+        if($student){
+            $student->name = $request->input('name');
+            $student->course = $request->input('course');
+            $student->email = $request->input('email');
+            $student->phone = $request->input('phone');
+
+            if($validator->fails()){
+                return response()->json([
+                    'validate_err' => $validator->messages(),
+                    'status' => 400,
+                ]);
+            }
+            else
+            {
+                $student->update();
+
+                return response()->json([
+                'status' => 200,
+                'message' => 'User Updated Successfully!',
+                ]); 
+            } 
+        }
+        else
+        {
+            return response()->json([
+                'status' => 404,
+                'message' => 'User does not exist!',
+            ]);
+        }
     }
 }
